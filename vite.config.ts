b06b47929444eff3a2fee88163056ab4d8dc751a
Shreyash Shigawan <shreyashshigwan1810@gmail.com>
2026-09-16 18:30:29 +0530

    fix(vite): optimize base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: process.env.VERCEL ? "/" : "./",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
